72ae2f2241a819ffde35658b4c7a027f05f36d88
lucasscabral <lucas.9cabral@gmail.com>
2024-03-21 16:08:50 -0300

    feat: Arquivos de utilidades do usuário

---
+++ teste-2_impetus/src/utils/userUtils.ts
@@ -0,0 +1,15 @@
+import * as bcrypt from "bcrypt";
+
+export async function hashPassword(password: string): Promise<string> {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
+
+export async function comparePasswords(
+  password: string,
+  hashedPassword: string
+): Promise<boolean> {
+  const match = await bcrypt.compare(password, hashedPassword);
+  return match;
+}
